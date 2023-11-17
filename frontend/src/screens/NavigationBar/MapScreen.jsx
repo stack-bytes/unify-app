6ab6828b2e7785a0a25107042418f500b0d2431d
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-11-17 16:13:36 +0200

    Basic navigation setup

---
+++ frontend/src/screens/NavigationBar/MapScreen.jsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+
+export default function MapScreen({navigation}){
+    return (
+        <View>
+            <Text>MapScreen</Text>
+        </View>
+    )
+}
