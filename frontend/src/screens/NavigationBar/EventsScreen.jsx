50c89e2eb5ff1cf8ad6153d6b868f009f4ea96e5
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-11-18 00:14:59 +0200

    Added dropdown menu to  the events interface

--- frontend/src/screens/NavigationBar/EventsScreen.jsx
+++ frontend/src/screens/NavigationBar/EventsScreen.jsx
@@ -1,11 +1,53 @@
-import { Image, ImageBackground, Text, View } from "react-native";
+import { FlatList, Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
 
 
 import GradientGreen from '../../../assets/backgrounds/GradientGreen.png';
 import { SearchBar } from "../../components/SearchBar";
 import { CurrentEvent } from "../../components/CurrentEvent";
 
+import ArrowIcon from "../../../assets/icons/arrow-icon.svg";
+import { TrendingIcon } from "../../components/trendingIcon";
+import { useState } from "react";
+
+const data = [
+    {
+        id: 1,
+        title: 'House Party',
+        description: 'This is the first event',
+        background: 'https://picsum.photos/200/300',
+        date: '2021-10-10',
+        time: '10:00',
+        location: '1234 Street',
+        category: 'Music',
+        trending: true,
+    },
+    {
+        id: 2,
+        title: 'House Party',
+        description: 'This is the first event',
+        background: 'https://picsum.photos/200/300',
+        date: '2021-10-10',
+        time: '10:00',
+        location: '1234 Street',
+        category: 'Music',
+        trending: true,
+    },
+    {
+        id: 3,
+        title: 'House Party',
+        description: 'This is the first event',
+        background: 'https://picsum.photos/200/300',
+        date: '2021-10-10',
+        time: '10:00',
+        location: '1234 Street',
+        category: 'Music',
+        trending: true,
+    },
+]
+
 export default function EventsScreen({navigation}){
+    const [isInEvent, setIsInEvent] = useState(true);
+    const [listIsOpen, setListIsOpen] = useState(false);
     return (
         <View className='w-full h-full bg-bg-dark'>
             <ImageBackground 
@@ -13,7 +55,7 @@ export default function EventsScreen({navigation}){
                 source={GradientGreen}
             />
             <View className='w-full h-full items-center top-20'>
-                <View className='flex items-center w-1/2 gap-y-2'>
+                <View className='flex items-center w-[100%] gap-y-2'>
                     <Text 
                         style={{fontFamily: 'SpaceGrotesk_500Medium'}}
                         className='text-text text-6xl'
@@ -21,7 +63,54 @@ export default function EventsScreen({navigation}){
                         Events
                     </Text>
                     <SearchBar></SearchBar>
-                    <CurrentEvent></CurrentEvent>
+                    
+                    {
+                        isInEvent && <CurrentEvent/>
+                    }
+
+                    <View className='bg-[#0E0D0D]/[0.27] border-2 border-[#0E0D0D]/[0.29] w-[90vw] h-16 rounded-lg'>
+                        <TouchableOpacity 
+                            className='w-full h-full absolute z-10'
+                            onPress={() => setListIsOpen(!listIsOpen)}
+                        />
+                        <View className='ml-4 h-full justify-start flex-row items-center '>
+                            <Text 
+                                style={{fontFamily: 'SpaceGrotesk_500Medium'}}
+                                className='text-text text-2xl pr-2'
+                            >
+                                In your area
+                            </Text>
+                            <ArrowIcon height='100%' fill='white'/>
+                        </View>
+                    </View>
+
+                    {
+                        listIsOpen &&
+                        <FlatList 
+                            className='h-36'
+                            data={data}
+                            keyExtractor={(item) => item.id}
+                            renderItem={({item}) => (
+                                <View className='bg-[#0E0D0D]/[0.27] border-2 border-[#0E0D0D]/[0.29] w-[90vw] h-16 rounded-lg justify-start flex-row pl-3 items-center mb-2'>
+                                    <TrendingIcon />
+                                    <View className='h-[70%] justify-center py-2'>
+                                        <Text 
+                                            style={{fontFamily: 'IBMPlexSans_700Bold'}}
+                                            className='text-text text-2xl pl-2'
+                                        >
+                                            {item.title}
+                                        </Text>
+                                        <Text 
+                                            style={{fontFamily: 'IBMPlexSans_400Regular'}}
+                                            className='text-primary text-lg pl-2'
+                                        >
+                                            {item.description}
+                                        </Text>
+                                    </View>
+                                </View>
+                            )}
+                        />
+                    }
                 </View>
             </View>
         </View>
