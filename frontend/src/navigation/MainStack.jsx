a0728466a71e669f589db52616e285bde5b95525
Andrei Dan <andrei.dan@moisenicoaraonline.ro>
2023-11-17 21:43:40 +0200

    some part of the genericButton

--- frontend/src/navigation/MainStack.jsx
+++ frontend/src/navigation/MainStack.jsx
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import { NavigationBar } from './NavigationBar';
 
+import PronounsScreen from '../screens/ProfileStack/PronounsScreen';
+
 const Stack = createStackNavigator();
 
 export const MainStack = ({ navigation }) => {
@@ -19,6 +21,11 @@ export const MainStack = ({ navigation }) => {
       <Stack.Screen name="NavigationBar" component={NavigationBar} options={{
            headerShown: false, 
            gestureEnabled: false,
+      }}/>
+
+      <Stack.Screen name="PronounsScreen" component={PronounsScreen} options={{
+        headerShown: false, 
+        gestureEnabled: false,
       }}/>
 
     </Stack.Navigator>
