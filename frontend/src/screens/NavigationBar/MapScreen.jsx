a15bd2158d07b146585349a3aad12c01a28be850
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-11-17 16:33:39 +0200

    SVG Implementation

--- frontend/src/screens/NavigationBar/MapScreen.jsx
+++ frontend/src/screens/NavigationBar/MapScreen.jsx
@@ -1,9 +1,14 @@
 import { View,Text } from "react-native";
 
+import FlagIcon from '../../../assets/icons/flag-icon.svg';
+
 export default function MapScreen({navigation}){
     return (
-        <View>
-            <Text>MapScreen</Text>
+        <View className='w-full h-full'>
+            <FlagIcon width='100%' height='80%' fill='black'/>
+            <Text className='text-3xl'>
+                This is a nice app
+            </Text>
         </View>
     )
 }
