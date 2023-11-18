195c6f43fc371887037a73ce8f73999876c2cdce
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-11-18 17:31:57 +0200

    user controller & routes (backend)

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,11 +3,12 @@ const router = Router();
 
 const eventsApi = require('./routes/eventsRoutes.js');
 const photosApi = require('./routes/photosRoutes.js');
-
+const userApi = require('./routes/userRoutes.js');
 
 
 router.use('/events', eventsApi);
 router.use('/photos', photosApi);
+router.use('/user', userApi);
 
 module.exports = router;
 
