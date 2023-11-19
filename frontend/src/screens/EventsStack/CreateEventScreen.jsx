69f73af1f1e85d6f9a8a499821515bfeb4025a0d
David-Andrei Popa <59252699+davidphex@users.noreply.github.com>
2023-11-19 02:20:30 +0200

    finished event creation

--- frontend/src/screens/EventsStack/CreateEventScreen.jsx
+++ frontend/src/screens/EventsStack/CreateEventScreen.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Text, View, Image, TextInput, FlatList, TouchableOpacity, Keyboard} from "react-native";
 import { GenericButton } from "../../components/Buttons/genericButton";
  
 import GradientGreen from "../../../assets/backgrounds/GradientGreen.png";
 
 import flagIcon from "../../../assets/icons/flag-icon.svg";	
+import ArrowIcon from "../../../assets/icons/arrow-icon.svg";
+import { UserContext } from "../../contexts/UserContext";
 
 
 export const CreateEventScreen = ({navigation, route}) => {
@@ -12,6 +14,28 @@ export const CreateEventScreen = ({navigation, route}) => {
     const [eventLocation, setEventLocation] = useState('');
     const [eventType, setEventType] = useState('');
 
+    const { user } = useContext(UserContext);
+
+    const createEvent = async () => {
+        fetch(`http://172.20.10.8:4949/api/events/createEvent`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userId: user.id,
+                name: eventName,
+                location: eventLocation,
+                type: eventType,
+                backgroundPic: 'https://res.cloudinary.com/dvbzt3rq8/image/upload/f_auto,q_auto/glqdszt99rlzgwkdwo93',
+                coords: user.location.coords,
+            })
+        })
+            .then(res => res.json())
+            .then(result => {
+                navigation.getParent().navigate('HomeStack');
+            })
+    }
     return (
         <View className='w-full h-full'>
             <View className = 'flex-1 items-center w-full'>
@@ -20,8 +44,20 @@ export const CreateEventScreen = ({navigation, route}) => {
                 className = 'bg-bg-dark w-full h-full absolute'/>
 
                 <Text className="text-4xl text-text mt-20">Create Event</Text>
-                
 
+                <TouchableOpacity 
+                    className='w-16 h-16 absolute left-4 top-12 '
+                    onPress={() => navigation.getParent().navigate('HomeStack')}
+                >
+                    <ArrowIcon 
+                        height='150%' width='50%' fill='white'
+                        style={{
+                            transform : [
+                                {rotate: '90deg'}
+                            ]
+                        }}
+                    />
+                </TouchableOpacity>
                 <View className = 'flex gap-y-4 h-[50px] w-[80%] mt-12'>
                     <Text className = 'text-[#10E3A5] text-[20px]'>Event name:</Text>
                     <View className = 'align-center justify-center h-full w-full border-[#D3D3D3]/[0.5] border-[1px] rounded-lg'>
@@ -49,8 +85,8 @@ export const CreateEventScreen = ({navigation, route}) => {
                                 marginLeft: 10,
                                 fontSize: 20,
                             }}
-                            onChangeText = {newText => setEventName(newText)}
-                            defaultValue = {eventName}
+                            onChangeText = {newText => setEventLocation(newText)}
+                            defaultValue = {eventLocation}
                             autoCapitalize="none"
                             returnKeyType = "done"
                             className = 'text-white'
@@ -66,8 +102,8 @@ export const CreateEventScreen = ({navigation, route}) => {
                                 marginLeft: 10,
                                 fontSize: 20,
                             }}
-                            onChangeText = {newText => setEventName(newText)}
-                            defaultValue = {eventName}
+                            onChangeText = {newText => setEventType(newText)}
+                            defaultValue = {eventType}
                             autoCapitalize="none"
                             returnKeyType = "done"
                             className = 'text-white'
@@ -80,6 +116,7 @@ export const CreateEventScreen = ({navigation, route}) => {
                             backgroundColor='rgba(22, 241, 228, 0.35)'
                             textColor={'#16F1E4'}
                             borderColor = {'#16F1E4'}
+                            onPress={createEvent}
                             
                     />
                     </View>
